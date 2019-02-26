80eb06e94e6715006b9657d85d8c6047e65a994d
winnie <winnie688801@gmail.com>
2019-02-26 11:59:36 +0800

    init

---
+++ myproject/src/com/ken/GraduateStudent.java
@@ -0,0 +1,12 @@
+package com.ken;
+
+import com.bear.Student;
+
+public class GraduateStudent extends Student{
+	public GraduateStudent() {
+		super("Ken ", 99 );
+		name = "John";
+		
+	}
+
+}
---
+++ myproject/src/com/ken/Tester.java
@@ -0,0 +1,12 @@
+package com.ken;
+
+import com.bear.Student;
+
+public class Tester {
+
+	public static void main(String[] args) {
+		Student stu = new Student("John", 98);
+		stu.setEnglish(100);
+	}
+
+}
