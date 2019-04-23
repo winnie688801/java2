0b4b84407396784f574b56db2000a21d621973b3
winnie688801 <winnie688801@gmail.com>
2019-04-23 12:00:49 +0800

    exception

--- myproject/src/com/test/Grade.java
+++ myproject/src/com/test/Grade.java
@@ -5,31 +5,15 @@ import java.util.Scanner;
 public class Grade {
 
 	public static void main(String[] args) {
-		int math = 0;
-		int english = 0;
 		Scanner scan = new Scanner(System.in);
 		System.out.print("Please enter your math score:");
-		try {
-			String ms = scan.nextLine();
-			math = Integer.parseInt(ms);
-		} catch (ArithmeticException e) {
-			System.out.println("The score should be 0~100.");
-		} catch (NumberFormatException e) {
-			System.out.println("The format is wrong.");
-		}
-		
+		String ms = scan.nextLine();
+		int math = Integer.parseInt(ms);
 		System.out.print("Please enter your english score:");
-		try {
-			String es = scan.nextLine();
-			english = Integer.parseInt(es);
-		} catch (ArithmeticException e) {
-			System.out.println("The score should be 0~100.");
-		} catch (NumberFormatException e) {
-			System.out.println("The format is wrong.");
-		}
-		
+		String es = scan.nextLine();
+		int english = Integer.parseInt(es);
 		int average = (math+english)/2;
-		System.out.println("Your average is " +average + ".");
+		System.out.println("Your average is " +average);
 	}
 
 }
